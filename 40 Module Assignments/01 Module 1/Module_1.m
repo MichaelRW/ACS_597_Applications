e38309200a117a0bb46c5f1ca26b46a9ec2b8dc3
Michael R. Wirtzfeld <michael.wirtzfeld@sonova.com>
2025-01-15 14:30:53 -0500

    Add lecture notes for Wednesday, January 15, 2025.

---
+++ 40 Module Assignments/01 Module 1/Module_1.m
@@ -0,0 +1,37 @@
+
+
+Send draft of report before submission for comments.
+
+
+% Problem 1
+
+% Fco = 858;  Hz
+
+
+% 0.5 *343 / 0.2 = 858
+
+
+Ly dimension is the largest dimension
+
+
+a.)  Dimensions used in anitmations.
+
+
+b.)  A = pir^2 = LxLy
+
+
+c.)  fco increases with water.
+
+
+d.)  
+
+
+Latex document.  Units not italized.  space between number and unit
+
+
+Focus on interpretation.
+
+
+Plot ideas:
+    1.)  Example:  Temperation, C;  Temperature [C] - y-axis and x-axis
+    2.)  
