3a90d323b28ac5cadaf2044d905b9c8d927061f9
Michael Wirtzfeld <michael.wirtzfeld@gmail.com>
2025-02-19 15:48:10 -0500

    Add lecture notes for Lecture 11 on Wednesday, February 19, 2025.

--- 40 Assignments/02 Module 2 Assignment/ACS_547_Module_2_Question_3_Wednesday_February_12_2025.m
+++ 40 Assignments/02 Module 2 Assignment/ACS_547_Module_2_Question_3_Wednesday_February_12_2025.m
@@ -3,7 +3,17 @@
 
 %% Synopsis
 
+% Question 3 - Transmission Loss Measurement
+
+
+% Reference(s):
+%
 % Slide 8 - Noise Reduction and Transmission Loss
+
+
+% Assumptions:
+%
+%   1.)  
 
 
 
@@ -29,16 +39,17 @@ PRINT_FIGURES = 0;
 
 
 
-%% Define Room and Panel
+%% Dimensions of Rooms and Panel
 
 room.length = 4;  % meters
 room.width = 4;  % meters
 room.height = 4;  % meters
-    room.volume = room.length * room.width * room.height;  % m^2
-    room.area = 2*(room.length * room.width) + 2*(room.length * room.height) + 2*(room.width * room.height);  % m^2
+    room.volume = room.length * room.width * room.height;  % 64 m^2
+    room.area = 2*(room.length * room.width) + 2*(room.length * room.height) + 2*(room.width * room.height);  % 96 m^2
 
 panel.width = 0.8;  % meters
 panel.height = 0.8;  % meters
+    panel.area = panel.width * panel.height;  % 0.64 m^2
 
 
 
@@ -49,18 +60,47 @@ T60 = [ 2.0  2.1  1.8  1.5  1.2  0.9 ].';  % seconds
 spl.source_room = [ 90  95  103  105  100  93 ].';  % dB re: 20e-6 Pascals
 spl.receiver_room = [ 50  50  46  50  50  38 ].';  % dB re: 20e-6 Pascals
 
+c = 343;  % meters per second
+
 
 
 %% Pressure Difference
 
 spl.delta = spl.source_room - spl.receiver_room;
 
-figure( ); ...
-    stem( octave_band_frequencies, spl.delta, 'Marker', '.', 'MarkerSize', 12, 'Color', 'r' );  grid on;
-    xlabel( 'Frequency [Hz]' );  ylabel( 'Transmission Loss [dB]' );
-    title( 'Pressure Difference Versus Octave Band Center Frequency' );
-    %
-    axis( [ 0 5e3  0 65 ] );
+% figure( ); ...
+%     stem( octave_band_frequencies, spl.delta, 'Marker', '.', 'MarkerSize', 12, 'Color', 'r' );  grid on;
+%     xlabel( 'Frequency [Hz]' );  ylabel( 'Transmission Loss [dB]' );
+%     title( 'Pressure Difference Versus Octave Band Center Frequency' );
+%     %
+%     xticks( octave_band_frequencies );  xticklabels( num2cell( octave_band_frequencies ) );
+%     set( gca, 'XScale', 'log' );
+%     xlim( [ 80 6e3 ] );  ylim( [ 0 65 ] );
+
+% https://www.mathworks.com/matlabcentral/answers/413686-how-to-set-log-scale-range
+
+
+
+%% Determine Average Absorption in the Receiver Room using Reverberation Time Measurements
+
+average_absorption = @( volume, area, c, T60 )  ( 55.25 .* volume ) ./ ( area .* c .* T60 );
+
+receiver_room.average_absorption = average_absorption( room.volume, room.area, c, T60 );
+
+% Assumption:  Calibration panel has very high transmission loss.
+
+% figure( ); ...
+%     stem( octave_band_frequencies, receiver_room.average_absorption, 'Marker', '.', 'MarkerSize', 12, 'Color', 'r' );  grid on;
+%     xlabel( 'Frequency [Hz]' );  ylabel( 'Average Absorption [Sabine]' );
+%     title( 'Average Absorption in Receiver Room Versus Octave Band Center Frequency' );
+%     %
+%     xticks( octave_band_frequencies );  xticklabels( num2cell( octave_band_frequencies ) );
+%     set( gca, 'XScale', 'log' );
+%     xlim( [ 80 6e3 ] );  ylim( [ 0 0.14 ] );
+
+return
+
+%% Determine the Transmission Loss in Each Octave Band
 
 return
 
