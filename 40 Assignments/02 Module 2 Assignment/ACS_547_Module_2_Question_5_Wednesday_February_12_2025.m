0f57941e0fce2f6d29a1f76f3b45147f5a32b894
Michael Wirtzfeld <michael.wirtzfeld@gmail.com>
2025-02-24 12:35:24 -0500

    Incremental commit.

--- 40 Assignments/02 Module 2 Assignment/ACS_547_Module_2_Question_5_Wednesday_February_12_2025.m
+++ 40 Assignments/02 Module 2 Assignment/ACS_547_Module_2_Question_5_Wednesday_February_12_2025.m
@@ -18,7 +18,7 @@ addpath( genpath( '../40 Assignments/00 Support' ), '-begin' );
 % set( 0, 'DefaultFigurePosition', [  400  400  900  400  ] );  % [ left bottom width height ]
 set( 0, 'DefaultFigurePaperPositionMode', 'manual' );
 set( 0, 'DefaultFigureWindowStyle', 'normal' );
-set( 0, 'DefaultLineLineWidth', 0.8 );
+set( 0, 'DefaultLineLineWidth', 1.2 );
 set( 0, 'DefaultTextInterpreter', 'Latex' );
 
 format ShortG;
@@ -41,16 +41,6 @@ machine.distance = 10;  % m
 
 octave_band_frequencies = [ 250  500  1000  2000  4000 ].';  % Hz
 Lw = [ 105  115  106  108  119 ].';  % dB re: 1 pW
-
-% figure( ); ...
-%     h1 = stem( octave_band_frequencies, Lw, 'Marker', '.', 'MarkerSize', 12, 'Color', 'r' );  hold on;
-%     h2 = line( [ 2e2 5e3 ], [ 30 30 ] );  grid on;
-%         legend( [ h1 h2 ], 'Current Sound Pressure Level', 'Target Sound Pressure Level', 'Location', 'North' );
-%     xlabel( 'Frequency [Hz]' );  ylabel( 'Sound Pressure Level [dB re: 20e-6 Pa]' );
-%     title( 'Sound Power Level Versus Octave Band Center Frequency' );
-%     %
-%     axis( [ 150 6e3  0 140 ] );
-%     set( gca, 'XScale', 'log' );
 
 
 
@@ -81,13 +71,7 @@ h_IL_large = @( Sw, alpha_w, Si, alpha_i, TL )  10*log10(  1  +  (Sw*alpha_w  + 
 enclosure.dimension = 2;  % m
     enclosure.area = 5 * enclosure.dimension^2;  % 20 m^2
 
-
-
-% https://www.cecoenviro.com/wp-content/uploads/2023/12/Acoustic-Enclosures-8pp-A4-web.pdf
-% https://www.controlnoise.com/product/acoustic-enclosures/
-
-
-% Volume of the enclosure is much bigger than the machine.  Diffuse sound field in the enclosure.
+% Volume of the enclosure is much bigger than the machine  Diffuse sound field in the enclosure.
 
 
 switch ( 4 )
@@ -228,23 +212,51 @@ switch ( 4 )
 end
 
 
-[ octave_band_IL    IL_estimates.'    (octave_band_IL - IL_estimates.') ]
 
+%% Calculate the Differences
 
-% What is the most restrictive case?
+% [ octave_band_IL    IL_estimates.'    (octave_band_IL - IL_estimates.') ]
+
+data = [ ...
+    250      44    28.0    19.7   10.6    5.6; ...
+    500      54    26.7    22.2    6.6    -4.4; ...
+    1000    45    5.6    1.4    -15.4    -22.4; ...
+    2000    47    -8.4    -2.2    -18.4    -19.4; ...
+    4000    58    -2.4    5.7     -11.0    -13.0 ];
+
+figure( ); ...
+    h1 = plot( data(:, 1 ), data( :, 3 ), 'LineStyle', '-', 'Color', 'k', 'Marker', '+', 'MarkerSize', 8 );  hold on;
+    h2 = plot( data(:, 1 ), data( :, 4 ), 'LineStyle', '--', 'Color', 'k', 'Marker', '+', 'MarkerSize', 8 );
+    h3 = plot( data(:, 1 ), data( :, 5 ), 'LineStyle', ':', 'Color', 'k', 'Marker', '+', 'MarkerSize', 8 );
+    h4 = plot( data(:, 1 ), data( :, 6 ), 'LineStyle', '-.', 'Color', 'k', 'Marker', '+', 'MarkerSize', 8 );  grid on;
+    line( [200 5500], [0 0 ], 'Color', 'r' );
+    legend( [ h1 h2 h3 h4 ], { 'Case 1 - Control Noise QBV 2', 'Case 2 - Control Noise QBV 3', 'Case 3 - Acoustics World HTL (100MM)', 'Case 4 - HTL 4 (class table)' }, 'Location', 'SouthOutside' );
+    xlabel( 'Frequency [Hz]' );  ylabel( 'Insertion Loss Difference [dB]' );
+    %
+    axis( [ 150 6e3  -30 30 ] );
+    set( gca, 'XScale', 'log' );
+    %
+    % Textheight:  744 pt. and Textwidth:  493 pt. from LaTex document
+    %
+    set( gcf, 'units', 'point', 'pos', [ 200 200    493*0.8 744*0.5 ] );
+        pos = get( gcf, 'Position' );
+            set( gcf, 'PaperPositionMode', 'Auto', 'PaperUnits', 'points', 'PaperSize', [pos(3), pos(4)] );
+    %             print(gcf, 'Q5 IL Plot', '-dpdf', '-r0' );
+%
+% https://tex.stackexchange.com/questions/179382/best-practices-for-using-matlab-images-in-latex
 
 
 
 %% Clean-up
 
-if ( ~isempty( findobj( 'Type', 'figure' ) ) )
-    monitors = get( 0, 'MonitorPositions' );
-        if ( size( monitors, 1 ) == 1 )
-            autoArrangeFigures( 2, 2, 1 );
-        elseif ( 1 < size( monitors, 1 ) )
-            autoArrangeFigures( 2, 2, 1 );
-        end
-end
+% if ( ~isempty( findobj( 'Type', 'figure' ) ) )
+%     monitors = get( 0, 'MonitorPositions' );
+%         if ( size( monitors, 1 ) == 1 )
+%             autoArrangeFigures( 2, 2, 1 );
+%         elseif ( 1 < size( monitors, 1 ) )
+%             autoArrangeFigures( 2, 2, 1 );
+%         end
+% end
 
 
 fprintf( 1, '\n\n\n*** Processing Complete ***\n\n\n' );
