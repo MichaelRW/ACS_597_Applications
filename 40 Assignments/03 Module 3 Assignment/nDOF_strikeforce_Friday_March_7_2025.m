90b1de4035cf2e0bb31c842b1a4d653368a1a84c
Michael Wirtzfeld <michael.wirtzfeld@gmail.com>
2025-03-07 16:12:40 -0500

    Incremental commit.

--- 40 Assignments/03 Module 3 Assignment/nDOF_strikeforce_Friday_March_7_2025.m
+++ 40 Assignments/03 Module 3 Assignment/nDOF_strikeforce_Friday_March_7_2025.m
@@ -36,6 +36,7 @@ dampings = [ 0  0  0 ];
 frequencies = 0:0.01:40;
 
 [ FRF ] = nDOF_direct_solution( masses, stiffnesses, dampings, frequencies, 'admittance' );  % 4001-by-2-by-2
+[ FRF ] = nDOF_direct_solution( masses, stiffnesses, dampings, frequencies, 'impedence' );  % 4001-by-2-by-2
 %
     % 'admittance'
     % 'mobility'
@@ -44,6 +45,21 @@ frequencies = 0:0.01:40;
     % 'impedance'
     % 'mass'
 
+
+admittance = zeros( numel( frequencies ), 1 );
+
+for index = 1:1:numel( frequencies )
+    temp = diag( squeeze( FRF( index, :, : ) ) );
+        temp2 = temp(1) + temp(2)*1j;
+        admittance( index ) = abs( temp2 );
+end
+
+clear temp1 temp2;
+
+
+figure( ); ...
+    semilogy( frequencies, admittance );  grid on;
+    xlabel( 'Frequency [Hz]' );  ylabel( 'Admittance [$\frac{m}{N}$]' );
 
 
 
