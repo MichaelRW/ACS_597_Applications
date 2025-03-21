5df85222f437c6d2bb6b3a40bea85d227b95d787
Michael Wirtzfeld <michael.wirtzfeld@gmail.com>
2025-03-21 11:37:34 -0400

    Incremental commit.

--- 40 Assignments/03 Module 3 Assignment/ACS_547_Module_3_Question_2_Monday_March_3_2025.m
+++ 40 Assignments/03 Module 3 Assignment/ACS_547_Module_3_Question_2_Monday_March_3_2025.m
@@ -5,11 +5,13 @@
 
 % Problem 2 - Washing Machine Two-stage Mount Design
 
-% Note(s):
-%
+
+
+%% Note(s):
+
 % With the force applied to the washing machine and the displacement of the
 % washing machine, use FSF( :, 1, 1 ).
-%
+
 % However, if the force is applied elsewhere, than FSF( :, 2, 2, ) might
 % need to be used.
 
@@ -26,7 +28,7 @@ addpath( genpath( './00 Support' ), '-begin' );
 % set( 0, 'DefaultFigurePosition', [  400  400  900  400  ] );  % [ left bottom width height ]
 set( 0, 'DefaultFigurePaperPositionMode', 'manual' );
 set( 0, 'DefaultFigureWindowStyle', 'normal' );
-set( 0, 'DefaultLineLineWidth', 1.5 );
+set( 0, 'DefaultLineLineWidth', 0.8 );
 set( 0, 'DefaultTextInterpreter', 'Latex' );
 
 format ShortG;
@@ -34,6 +36,97 @@ format ShortG;
 pause( 1 );
 
 
+
+%% Problem 2a
+
+% Use Visio to draw diagram of the two-stage mount.  Include dash-pots.
+
+
+
+%% Problem 2b
+
+% The frequency of the forcing function is 5 Hz and the amplitude is 494.4 N.
+fo = 5;  % Hz
+    wo = 2*pi*fo;  % 31.4 radians\s
+
+
+m1 = 100 + 10;  % kg - Total mass of washing machine and its load.
+    k1 = 5e3;  % N\m
+    c1 = 10;
+
+m2 = 100;  % kg - UPPER LIMIT
+    k2 = 6e3;  % N\m
+    c2 = 10;
+
+k3 = 100e3;
+
+
+w1 = sqrt( ( k1 + k3 ) / m1 );  % 30.9 radians\s
+
+w2 = sqrt( ( k2 + k3 ) / m2 );  % 32.6 radians\s
+
+% return
+
+%% Problem 2c
+
+masses = [ m2 m1 ];
+stiffnesses = [ k2  k3  k1 ];
+dampings = [ c2  0  c1 ];
+
+frequencies = 0:0.01:40;
+
+FRF = nDOF_direct_solution( masses, stiffnesses, dampings, frequencies, 'admittance' );
+
+
+admittance = zeros( numel( frequencies ), 1 );
+
+for index = 1:1:numel( frequencies )
+    temp = diag( squeeze( FRF( index, 2, 2 ) ) );
+        admittance( index ) = abs( temp );
+end
+%
+clear temp temp2;
+
+washing_machine.admittance = admittance;
+
+
+admittance = zeros( numel( frequencies ), 1 );
+
+for index = 1:1:numel( frequencies )
+    temp = diag( squeeze( FRF( index, 2, 1 ) ) );
+        admittance( index ) = abs( temp );
+end
+%
+clear temp temp2;
+
+raft.admittance = admittance;
+
+
+figure( ); ...
+    h1 = loglog( frequencies, washing_machine.admittance );  hold on;
+    h2 = loglog( frequencies, raft.admittance );  grid on;
+        legend( [ h1 h2 ], 'Washing Machine', 'Raft', 'Location', 'SouthWest' );
+    %
+    line( [ 5 5 ], [ 10^-8 10^-2 ] );
+    xlabel( 'Frequency [Hz]' );  ylabel( 'Admittance [$\frac{m}{N}$]' );
+
+return
+
+%% Problem 2d
+
+return
+
+%% Problem 2e
+
+return
+
+%% Problem 2f
+
+return
+
+%% Problem 2g
+
+return
 
 %% 1 DOF Example
 
@@ -110,97 +203,6 @@ figure( 'Name', 'Dynamic Stiffness' ); ...
     semilogy( frequencies, 1 ./ abs( FRF( :, 2, 2 ) ) );  grid on;
         legend( 'M1 Movement with F1', 'M1 Movement with F2', 'M2 Movement with F1', 'M2 Movement with F2', 'Location', 'North' );
     xlabel( 'Frequency [Hz]' );  ylabel( 'Admittance [$\frac{m}{N}$]' );
-
-
-
-%% Clean-up
-
-if ( ~isempty( findobj( 'Type', 'figure' ) ) )
-    monitors = get( 0, 'MonitorPositions' );
-        if ( size( monitors, 1 ) == 1 )
-            autoArrangeFigures( 3, 4, 1 );
-        elseif ( 1 < size( monitors, 1 ) )
-            autoArrangeFigures( 2, 2, 2 );
-        end
-end
-
-fprintf( 1, '\n\n\n*** Processing Complete ***\n\n\n' );
-
-
-
-%% Reference(s)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
