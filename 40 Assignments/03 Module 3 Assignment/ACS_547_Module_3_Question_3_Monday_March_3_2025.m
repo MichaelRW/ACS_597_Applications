b225984cb8d3a10a2e60c4d4d472ebb547eacf9d
Michael Wirtzfeld <michael.wirtzfeld@gmail.com>
2025-03-29 08:59:42 -0400

    Incremental commit.

--- 40 Assignments/03 Module 3 Assignment/ACS_547_Module_3_Question_3_Monday_March_3_2025.m
+++ 40 Assignments/03 Module 3 Assignment/ACS_547_Module_3_Question_3_Monday_March_3_2025.m
@@ -55,6 +55,7 @@ h_f_to_rpm = @( f )  ( f*2*pi ) / 0.10471;
 h_w_to_rpm = @( w )  h_f_to_rpm( w*2*pi);
 h_w_to_f = @( w )  w/(2*pi);
 h_rpm_to_f = @( rpm )  ( rpm * 0.10471 ) / ( 2 * pi );
+h_rpm_to_w = @( rpm )  ( rpm * 0.10471 );
 
 h_force = @( force_mass, rotation_speed, load_distance  )  force_mass .* rotation_speed.^2 .* load_distance;
 
@@ -91,37 +92,35 @@ wo = sqrt( k / m);  % 9.4 radians\s
     fo = wo/(2*pi);  % 1.5 Hz
 
 
-% Add a Dynamic Vibration Absorber (DVA)
 m_dva = 15;  % kg
-% % m_dva = 1;  % kg
-
+k_dva = 1e5;  % N\m
+% k_dva = 2e5;  % N\m
 % k_dva = 1800;  % N\m ->  7.7095e-05;  2.4411e-05;  0.23947 ->  f2 IS LESS THAN f1
-k_dva = 1000;  % N\m ->  7.2494e-05;  2.2954e-05;  0.22518
+% k_dva = 1000;  % N\m ->  7.2494e-05;  2.2954e-05;  0.22518
 % k_dva = 100;  % N\m ->  6.8007e-05;  2.1533e-05;  0.21124
 % k_dva = 10;  % N\m ->  6.7593e-05;  2.1402e-05;  0.20995
 % k_dva = 1;  % N\m ->  6.7552e-05;  2.1389e-05;  0.20983
 
-wo_dva = sqrt( k_dva / m_dva );  % 8.2 radians\s
-    fo_dva = wo_dva/(2*pi);  % 1.3 Hz
+wo_dva = sqrt( k_dva / m_dva );  % 81.7 radians\s
+    fo_dva = wo_dva/(2*pi);  % 13.0 Hz
 
-% return
 
-%% Blocked Frequencies (Euqation 9.1 of the Notes)
+
+%% Blocked Frequencies (Equation 9.1 of the Notes)
 
 m1 = m_dva;
 k1 = 0;  % Spring above DVA mass;  does not exist;  set to zero.
 
 m2 = m;
-k2 = k;  %  N/m
+k2 = k;
 
-k3 = k_dva;  % 1,800 N/m
+k3 = k_dva;
 
 
-w1 = sqrt( ( k1 + k3 ) / m1 );  % 10.95 radians\s;  smaller than the system resonant frequency.
+w1 = sqrt( ( k1 + k3 ) / m1 );  % 81.6 radians\s;  smaller than the system resonant frequency.
     f1 = w1/(2*pi);  % 1.3 Hz
 
-
-w2 = sqrt( ( k2 + k3 ) / m2 );  % 10.27 radians\s;  greater than the system resonant frequency.
+w2 = sqrt( ( k2 + k3 ) / m2 );  % 31.6 radians\s;  greater than the system resonant frequency.
     f2 = w2/(2*pi);  % 1.6 Hz
 
 % return
@@ -164,18 +163,17 @@ phi_minus = [ ...
 
 %% Plot (1) Admittance Using Equations from DVA Lecture
 
+MAXIMUM_RPM = 3e3;
+
 F0 = 1;
 
-w = 0:0.01:37;  % 37 radians\s is 350 RPM
+w = 0:0.01:h_rpm_to_w( MAXIMUM_RPM );
     f = w/(2*pi);
         rpm = h_f_to_rpm( f );
 
 
 m = [ 15 110 ];
-k = [  0  k_dva  k2 ];
-% k = [  0  k_dva.*(1+0.1*1i)  k2 ];
-% k = [  0  k_dva  k2.*(1+0.1*1i)  ];
-dampings = [ 0 0 0 ];
+k = [  0  k_dva  k2.*(1+0.2*1i)  ];
 
 admittance_modified = F0 ./ ( m(1)*m(2) )*k(2) ./ (( w.^2 - w_plus^2 ) .* ( w.^2 - w_minus^2) );
 admittance_original = F0 ./ m(2) * 1 ./ ( w2^2 - w.^2 );
@@ -193,7 +191,7 @@ figure( 'Name', 'Admittance - Equations from DVA Lecture' ); ...
     
     h7 = line( [ h_f_to_rpm( f_plus )  h_f_to_rpm( f_plus ) ], [ 1e-7 1e3 ], 'Color', [ 0.72, 0.27, 1.00 ], 'LineStyle', '--' );    grid on;
     
-    axis( [ 2e1 4e2  1e-7 2e2 ] );
+    axis( [ 5 3e3  1e-7 1 ] );
         legend( [ h1 h2 h3 h4 h5 h6 h7 ], 'Original System', 'DVA System', 'Operating RPM', ...
             '$w_-$', '$w_1$', ...
             '$w_2$', '$w_+$', ...
@@ -208,11 +206,11 @@ figure( 'Name', 'Admittance - Equations from DVA Lecture' ); ...
 % value to the respective spring stiffness.  THESE ARE NOT INTERCHANGABLE.
 
 
-rpm = 0:0.1:350;
+rpm = 0:0.1:MAXIMUM_RPM;
     f = h_rpm_to_f( rpm );
         w = f ./ (2*pi);
 
-FRF = nDOF_direct_solution( m, k, dampings, f, 'admittance' );  % Symmetric matrix.
+FRF = nDOF_direct_solution( m, k, [ 0 0 0 ], f, 'admittance' );  % Symmetric matrix.
 %
 squeeze( FRF( numel( f ), :, :) );
 %
@@ -236,7 +234,7 @@ figure( 'Name', 'Admittance - nDOF Calculated' ); ...
     
     h6 = line( [ h_f_to_rpm( f_plus )  h_f_to_rpm( f_plus ) ], [ 1e-7 1e3 ], 'Color', [ 0.72, 0.27, 1.00 ], 'LineStyle', '--' );    grid on;
     
-    axis( [ 2e1 4e2  1e-7 2e2 ] );
+    axis( [ 2e1 MAXIMUM_RPM  1e-7 1 ] );
         legend( [ h1 h2 h3 h4 h5 h6 ], 'DVA System', 'Operating RPM', ...
             '$w_-$', '$w_1$', ...
             '$w_2$', '$w_+$', ...
@@ -271,9 +269,13 @@ figure( 'Name', 'Displacement' ); ...
     
     h7 = line( [ h_f_to_rpm( f_plus )  h_f_to_rpm( f_plus ) ], [ 1e-8 1e3 ], 'Color', [ 0.72, 0.27, 1.00 ], 'LineStyle', '--' );    grid on;
         
-        legend( [ h1  h2 ], 'Washing Machine', 'Load Frequency', 'Location', 'Northwest' );
+        legend( [ h1 h2 h3 h4 h5 h6 ], 'DVA System', 'Operating RPM', ...
+            '$w_-$', '$w_1$', ...
+            '$w_2$', '$w_+$', ...
+            'Location', 'NorthWest', 'Interpreter', 'Latex' );
+
     xlabel( 'Rotation [RPM]' );  ylabel( 'Displacment [m]' );
-    axis( [ 2e1 400  1e-8 1e1 ] );
+    axis( [ 2e1 MAXIMUM_RPM  1e-8 1 ] );
 
 
 temp2 = abs( FRF( :, 1, 1 ) ).*temp;
@@ -289,15 +291,15 @@ dynamic_force.radius = 0.5;  % m
 
 temp = h_force( dynamic_force.mass, w, dynamic_force.radius ).';
 
-
+close all
 figure( 'Name', 'Ground Force' ); ...
     h1 = loglog( rpm, abs( FRF( :, 1, 1 ) ).*temp.*k2, 'Color', 'k' );  hold on;
-    h2 = line( [ 300 300 ], [ 1e-2 1e-0 ], 'Color', 'k', 'LineStyle', '--' );  grid on;
+    h2 = line( [ 300 300 ], [ 1e-4 1e-0 ], 'Color', 'k', 'LineStyle', '--' );  grid on;
         text( 220, 2e-0, '300 RPM' );
-    h3 = line( [ 6 400 ], [ 100 100 ], 'Color', 'r' );
-        legend( [ h1 h2 h3 ], 'Ground Force', 'Operating RPM', 'Maximum Floor Load', 'Location', 'South' );
+    h3 = line( [ 6 MAXIMUM_RPM ], [ 100 100 ], 'Color', 'r' );
+        legend( [ h1 h2 h3 ], 'Ground Force', 'Operating RPM', 'Maximum Floor Load', 'Location', 'SouthEast' );
     xlabel( 'Rotation [RPM]' );  ylabel( ' Force [N]' );
-    axis( [ 2e1 400  1e-5 1e3 ] );
+    axis( [ 2e1 MAXIMUM_RPM  1e-5 1e3 ] );
 
 
 temp2 = abs( FRF( :, 1, 1 ) ).*temp*k2;
