8111ee9cdcc3ddf04edaa5100c2cf1ba0c1c7c7d
Michael Wirtzfeld <michael.wirtzfeld@gmail.com>
2025-04-13 20:15:27 -0400

    Incremental commit.

--- 40 Assignments/04 Module 4 Assignment/color_strikeforce_Sunday_April_13_2025.m
+++ 40 Assignments/04 Module 4 Assignment/color_strikeforce_Sunday_April_13_2025.m
@@ -30,3 +30,17 @@ end
 figure( ); ...
     plot( x, y );
         colororder( cmap_vectors )
+
+
+
+% https://www.mathworks.com/matlabcentral/answers/507355-colormap-for-multline-plot
+
+% https://www.mathworks.com/matlabcentral/answers/405484-how-to-get-handles-of-the-line-object
+
+% https://www.mathworks.com/matlabcentral/answers/2091796-saving-multiple-plots-into-one-handle
+
+% https://www.mathworks.com/matlabcentral/answers/296360-how-can-i-change-the-line-color-e-g-color-g-doesn-t-work
+
+% https://www.mathworks.com/matlabcentral/answers/438317-how-to-color-individual-lines-in-a-plot
+
+
