8005b8202e2ffff623cc02e5d661a96618189f6e
Michael Wirtzfeld <michael.wirtzfeld@gmail.com>
2025-04-23 17:11:18 -0400

    Incremental commit.

--- 40 Assignments/05 Module 5 Assignment/ACS_547_Module_5_Question_5_Sunday_April_20_2025.m
+++ 40 Assignments/05 Module 5 Assignment/ACS_547_Module_5_Question_5_Sunday_April_20_2025.m
@@ -23,10 +23,10 @@ close all; clear; clc;
 
 addpath( genpath( './00 Support' ), '-begin' );
 
-% set( groot, 'DefaultFigurePosition', [ 1.7e3  775    750  500 ] );  % x, y, width, height
+set( groot, 'DefaultFigurePosition', [ 230 730  750  500 ] );  % x, y, width, height
 
 set( 0, 'DefaultFigurePaperPositionMode', 'manual' );
-set( 0, 'DefaultFigureWindowStyle', 'docked' );
+set( 0, 'DefaultFigureWindowStyle', 'normal' );
 set( 0, 'DefaultLineLineWidth', 1.0 );
 set( 0, 'DefaultTextInterpreter', 'Latex' );
 
@@ -76,7 +76,26 @@ smoothed_trigger_data = [ 0;  diff( smooth( trigger, 'moving', 5 ) ) ];
         lead_edge = edge_indices( 1:2:end );
 
 
+lead_edge( 1:10 )        
+
+temp = diff( lead_edge );
+    temp( 1:10 )
+
+[ indices_to_remove, indices ] = find( temp == 2 );    
+
+temp2 = lead_edge;
+    temp2( indices_to_remove ) = [ ];
+
+temp2;    
+
+% figure;  plot( temp2 );
+
+
+
+
 theta = [ ]; 
+
+start_index = [ ];  end_index = [ ];
 
 % Linear interpolation for each interval between the leading edges.
 for index = 1:1:numel( lead_edge )
@@ -89,6 +108,8 @@ for index = 1:1:numel( lead_edge )
         x_difference = lead_edge( index );
     else
         x_difference = lead_edge( index ) - lead_edge( index - 1 );
+            start_index = [ start_index  lead_edge( index - 1 ) ];
+            end_index = [ end_index  lead_edge( index )  ];
     end    
     
     slope = y_difference / x_difference;
@@ -107,8 +128,8 @@ figure( ); ...
     h1 = subplot( 2, 1, 1 ); ...
         plot( trigger( 1:1:end ) );  hold on;
         %
-        for index = 1:1:numel(lead_edge)
-            line( [ lead_edge(index) lead_edge(index)], [ 0 4 ], 'Color', 'r' );  grid on;
+        for index = 1:1:numel(temp2)
+            line( [ temp2(index) temp2(index)], [ 0 4 ], 'Color', 'r' );  grid on;
         end
 
     h2 = subplot( 2, 1, 2 ); ...
@@ -116,9 +137,9 @@ figure( ); ...
     %
     linkaxes( [ h1 h2 ], 'xy' );
         axis( [ 1 3e5   0 6.5 ] );
+        % axis( [ 1 numel( trigger )   0 6.5 ] );
 
-return
-
+% return
 
 %% Compute Fourier Coefficients Directly for Each Revolution
 
@@ -135,26 +156,27 @@ return
 % soundsc( p( :, 11 ), fs );  % -32 degrees
 
 
+temp2
 
-signal = p(:, 1);
-% signal = p(:, 5);
-
-frame_length = 2048;  frame_hop = 1024;
-
-APPLY_HANN_WINDOW = 1;
-    spectrogram_mrw = spectrogram_September_26_2023( signal, frame_length, frame_hop, fs, APPLY_HANN_WINDOW );
-
-figure( ); ...
-    h_1 = imagesc( spectrogram_mrw.time_indices, spectrogram_mrw.Sxx.frequencies, 10*log10( spectrogram_mrw.Sxx.spectrum ), [ -100 -10 ] );
-        labelColorbar( 'PSD [dB re: 1 $\frac{Volts^2}{Hz}$]' );  grid on;
-        colormap parula;  % Option:  Turbo
-        set( gca, 'GridColor', 'w', 'GridAlpha', 0.3 );
-    title( 'Order Tracking', 'Interpreter', 'Latex' );
-    xlabel( 'Time [seconds]', 'Interpreter', 'Latex' );  ylabel( 'Frequency [Hz]', 'Interpreter', 'Latex' );
-    set( gca, 'ydir', 'normal' );
-    ylim( [ 0 fs/2] );
-    % datatip( h_1, 2, +128 );
-    % datatip( h_1, 2, -128, 'Location', 'southeast' );
+% signal = p(:, 1);
+% % signal = p(:, 5);
+% 
+% frame_length = 2048;  frame_hop = 1024;
+% 
+% APPLY_HANN_WINDOW = 1;
+%     spectrogram_mrw = spectrogram_September_26_2023( signal, frame_length, frame_hop, fs, APPLY_HANN_WINDOW );
+% 
+% figure( ); ...
+%     h_1 = imagesc( spectrogram_mrw.time_indices, spectrogram_mrw.Sxx.frequencies, 10*log10( spectrogram_mrw.Sxx.spectrum ), [ -100 -10 ] );
+%         labelColorbar( 'PSD [dB re: 1 $\frac{Volts^2}{Hz}$]' );  grid on;
+%         colormap parula;  % Option:  Turbo
+%         set( gca, 'GridColor', 'w', 'GridAlpha', 0.3 );
+%     title( 'Order Tracking', 'Interpreter', 'Latex' );
+%     xlabel( 'Time [seconds]', 'Interpreter', 'Latex' );  ylabel( 'Frequency [Hz]', 'Interpreter', 'Latex' );
+%     set( gca, 'ydir', 'normal' );
+%     ylim( [ 0 fs/2] );
+%     % datatip( h_1, 2, +128 );
+%     % datatip( h_1, 2, -128, 'Location', 'southeast' );
 
 
     
